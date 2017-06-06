c90ae2f5178340b82665ad2245951b58ae7ceee5
Alex Craven <Alexcraven00@users.noreply.github.com>
2017-06-06 15:19:45 +0100

    Updated basic layout and changed extension to PHP
    Updated the basic layout to the newer version and changed file extension to index.php.
    This will enable the site to be more dynamic.

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BecomingMe</title>
+	<link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<div class="Website container">
+  <div class="head">
+ <nav id="nav" class="navbar">
+  <ul>
+   <li class="active"><a href="index.php">BecomingMe</a></li>
+   <li style="float:right"><a href="login.php">Login</a></li>
+   <li style="float:right"><a href="register.php">Register</a></li>
+   </hr>
+  </ul>
+  </nav>
+  
+  </div>
+  <div class="main">
+  <section class="">
+    </hr>
+    <center><h2>Becoming Me</h2>
+    <h3>Have you ever wanted to talk about your mental health experiences either posted on your own page or anonymously?</h3>
+    <h2>Becoming Me is just the website for you!</h2>
+      </hr>
+    
+   </center>
+    </section>
+  <section class="">
+    <h3>Sign up below for updates</h3>
+    <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="checkbox">
+    <label>
+      <input type="checkbox"> Check me out
+    </label>
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+</section>
+  </div>
+  <div class="footer">
+    <center><h4>Becoming Me </h4></center>
+     <center>
+     <div class="github">
+   <li id="github">
+     <a href="https://github.com/Alexcraven00/BecomingMe">
+    <i class="fa fa-2x fa-github" onmouseover="document.getElementById('github').style.color = 'blue'"
+onmouseout="document.getElementById('github').style.color = 'white'" ></i>
+       </a>
+    </li>
+       <label><a href="https://github.com/Alexcraven00/BecomingMe">Visit our github </a></label>
+       </div>
+    <div class="facebook">
+    <center>
+   
+<li id="facebook">
+  <i class="fa fa-facebook-official fa-2x"       onmouseover="document.getElementById('facebook').style.color = 'blue'"
+onmouseout="document.getElementById('facebook').style.color = 'white'" 
+></i>
+ </li>
+         <label>Visit our Facebook</lable>
+      </center>
+       </div>
+    
+</div>
+
+</div>
+</body>
+</html>
