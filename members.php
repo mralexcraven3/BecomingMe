18f82e30ad4141c3024b88a0e789d56bc2785e43
Alex Craven <Alexcraven00@users.noreply.github.com>
2017-06-07 10:18:12 +0100

    Created members.php
    Moved from master branch to dev branch as members.php is in development.

---
+++ members.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BecomingMe</title>
+	<link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+</head>
+<body>
+<div class="Website container">
+  <div class="head">
+ <nav id="nav" class="navbar">
+  <ul>
+   <li class="active"><a href="index.php">BecomingMe</a></li>
+   <li style="float:right"><a href="login.php">Login</a></li>
+   <li style="float:right"><a href="register.php">Register</a></li>
+   </hr>
+  </ul>
+  </nav>
+  
+  </div>
+  <div class="main">
+  <h1>Welcome to our website!</h1>
+  </hr>
+  </div>
+  <div class="footer">
+    <center><h4>Becoming Me </h4></center>
+     <center>
+     <div class="github">
+   <li id="github">
+     <a href="https://github.com/Alexcraven00/BecomingMe">
+    <i class="fa fa-2x fa-github" onmouseover="document.getElementById('github').style.color = 'blue'"
+onmouseout="document.getElementById('github').style.color = 'white'" ></i>
+       </a>
+    </li>
+       <label><a href="https://github.com/Alexcraven00/BecomingMe">Visit our github </a></label>
+       </div>
+    <div class="facebook">
+    <center>
+   
+<li id="facebook">
+  <i class="fa fa-facebook-official fa-2x"       onmouseover="document.getElementById('facebook').style.color = 'blue'"
+onmouseout="document.getElementById('facebook').style.color = 'white'" 
+></i>
+ </li>
+         <label>Visit our Facebook</lable>
+      </center>
+       </div>
+    
+</div>
+
+</div>
+<script src="https://use.fontawesome.com/57e7668e7f.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.1/angular.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+</body>
+</html>
